34495d8a7a34f67ca936572b05eb1f935764f26e
bobbyhadz <opmftw@gmail.com>
2024-04-09 16:43:13 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Print a zipped list in Python
+
+list_1 = [1, 2, 3]
+list_2 = ['bobby', 'hadz', 'com']
+
+my_list = list(zip(list_1, list_2))
+
+# 👇️ [(1, 'bobby'), (2, 'hadz'), (3, 'com')]
+print(my_list)
